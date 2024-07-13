61cb758a3b7c0c112f086adf8a0744ed3507d908
US <79787726+UnfiniSmile@users.noreply.github.com>
2024-07-14 02:01:16 +0800

    Update BiliRefine.js

--- BiliRefine.js
+++ BiliRefine.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         哔哩哔哩视频列表增强
-// @version      240714
+// @version      240714.1
 // @description  修改视频合集长度_直播视频大小_评论区间隔
 // @author       未完待笑(UnfiniSmile)
 // @copyright    2024, UnfiniSmile (https://github.com/UnfiniSmile)
